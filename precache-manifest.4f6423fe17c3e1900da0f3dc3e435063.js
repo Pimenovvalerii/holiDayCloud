fd48ec5c9414939f6cebb199c0260a6831ba6136
Pimenovvalerii <Pimenovvalerii92@gmail.com>
2019-11-11 19:28:24 +0200

    Updates

---
+++ precache-manifest.4f6423fe17c3e1900da0f3dc3e435063.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ab551e28c46af969a262757ddf5d2f",
+    "url": "/holiDayCloud/index.html"
+  },
+  {
+    "revision": "2803d8ac7ae27e46effe",
+    "url": "/holiDayCloud/static/css/main.de28a9b3.chunk.css"
+  },
+  {
+    "revision": "a1f515069b1c4d15c63c",
+    "url": "/holiDayCloud/static/js/2.a56b653b.chunk.js"
+  },
+  {
+    "revision": "2803d8ac7ae27e46effe",
+    "url": "/holiDayCloud/static/js/main.3b9637b9.chunk.js"
+  },
+  {
+    "revision": "156247f3d5357177c46e",
+    "url": "/holiDayCloud/static/js/runtime-main.2f1595f3.js"
+  }
+]);
