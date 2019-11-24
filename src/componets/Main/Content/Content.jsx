364215548224b1754818a2e9ccbfa17047d36dbd
Pimenovvalerii <Pimenovvalerii92@gmail.com>
2019-11-24 13:37:04 +0200

    content changes

--- src/componets/Main/Content/Content.jsx
+++ src/componets/Main/Content/Content.jsx
@@ -10,31 +10,22 @@ import holidays from './holidays';
 export default class Content extends React.Component {
 
     render(){
-            const today = new Date();
-            today.setHours(0);
-            today.setMinutes(0);
-            today.setSeconds(0);
-            today.setMilliseconds(0);
+        const today = new Date();
+        today.setHours(0);
+        today.setMinutes(0);
+        today.setSeconds(0);
+        today.setMilliseconds(0);
 
-            const events = this.props.user
-                ? this.props.user.array.concat(holidays)
-                : holidays;
+        const events = this.props.user
+            ? this.props.user.array.concat(holidays)
+            : holidays;
 
-                events.sort((a, b)=>{
-                        const d1 = new Date(a.data).setFullYear(today.getFullYear())
-                        const d2 = new Date(b.data).setFullYear(today.getFullYear())
-                        const dif1 = d1-today > 0 ? d1-today : d1-today + 31536000000
-                        const dif2 = d2-today > 0 ? d2-today : d2-today + 31536000000
-                    return dif1 - dif2
-                })
-
-<<<<<<< HEAD
-            const sortedArr = []
-            
-            Object.keys(dates).sort(function compareNumbers(a, b) {
-                return a - b;
-              }).forEach((el,i)=>{
-                sortedArr.push(dates[el])
+            events.sort((a, b)=>{
+                    const d1 = new Date(a.data).setFullYear(today.getFullYear())
+                    const d2 = new Date(b.data).setFullYear(today.getFullYear())
+                    const dif1 = d1-today > 0 ? d1-today : d1-today + 31536000000
+                    const dif2 = d2-today > 0 ? d2-today : d2-today + 31536000000
+                return dif1 - dif2
             })
 
             let settings = {
@@ -70,25 +61,14 @@ export default class Content extends React.Component {
                 <div className="slick">
                     <Slider {...settings}>
                         
-                        <ContentItem event={sortedArr[0]} />
-                        <ContentItem event={sortedArr[1]} />
-                        <ContentItem event={sortedArr[2]} />
+                        <ContentItem event={events[0]} />
+                        <ContentItem event={events[1]} />
+                        <ContentItem event={events[2]} />
 
                     </Slider>
-=======
-            return (
-                <div className="content">
-                    <ContentItem event={events[0]} />
-                    <ContentItem event={events[1]} />
-                    <ContentItem event={events[2]} />
->>>>>>> 0ddba8713000ea1521cbb9be8720671920250798
+
                 </div>
                 
-                // <div className="content">
-                //     <ContentItem event={sortedArr[0]} />
-                //     <ContentItem event={sortedArr[1]} />
-                //     <ContentItem event={sortedArr[2]} />
-                // </div>
             )
         
 
